b0cac8f1a196fbc1bdcd87b0e93c804562ead7ad
Leninner <Mazabandalenin180@gmail.com>
2021-04-28 19:17:01 -0500

    Agregamos más interactividad a la calculadora

--- juice.js
+++ juice.js
@@ -1,10 +1,5 @@
-const simSuma = "+";
-const simRetsa = "-";
-const simMultiplicar = "*";
-const simDividir = "/";
-const result1 = document.querySelector("#result1");
-const result2 = document.querySelector('#result2');
 const val = document.querySelector('#valores');
+const result = document.querySelector('#resultado');
 //Consultamos números y agregamos a la pantalla de la calculadora al hacer click
 const one = document.querySelector('#uno');
 const two = document.querySelector('#dos');
@@ -23,80 +18,52 @@ const divide = document.querySelector('#dividir');
 const borrar = document.querySelector('#reset');
 const igual = document.querySelector('#equal');
 
-
 switch(one){
-    case one: numero(one);
-    case two: numero(two);
-    case three: numero(three);
-    case four: numero(four);
-    case five: numero(five);
-    case six: numero(six);
-    case seven: numero(seven);
-    case eight: numero(eight);
-    case nine: numero(nine);
-    case puntar: numero(puntar);
-    case sumar: operando(sumar);
-    case restar: operando(restar);
-    case multiplica: operando(multiplica);
-    case divide: operando(divide);
-    case borrar: resetear(borrar);
-    //TODO: Crear función para que al dar click aquí, aparezca el resultado.
-    case igual: operando(igual);
+     case one: numero(one);
+     case two: numero(two);
+     case three: numero(three);
+     case four: numero(four);
+     case five: numero(five);
+     case six: numero(six);
+     case seven: numero(seven);
+     case eight: numero(eight);
+     case nine: numero(nine);
+     case puntar: numero(puntar);
+     case sumar: operando(sumar);
+     case restar: operando(restar);
+     case multiplica: operando(multiplica);
+     case divide: operando(divide);
+     case borrar: resetear(borrar);
 }
 
-//Objeto que contiene números
-const numeros = {
-    uno: one.textContent,
-    dos: two.textContent,
-    tres: three.textContent,
-    cuatro: four.textContent,
-    cinco: five.textContent,
-    seis: six.textContent,
-    siete: seven.textContent,
-    ocho: eight.textContent,
-    nueve: nine.textContent,
-}
-console.table(numeros);
+igual.addEventListener('click', () => {
+    result.textContent = val.textContent;
+    console.log(result);
+});
 
+let val1 = 0;
+function numero(number){
+    number.addEventListener('click', (e) => {
+        val.textContent += e.target.textContent;
+        val1 = parseFloat(val.textContent);
+        console.log(val1);
+    });
+}
 
 //Funcion para resetear
 function resetear(valor){
     valor.addEventListener('click', () => {
-        valores.textContent  = "";
+        val.textContent  = "";
+        result.textContent = "0";
     });
 }
 
 //Funcion para escribir el operador
 function operando(o){
-  o.addEventListener('click', (e) => {
-    valores.textContent = o.textContent;
-  });
-}
-
-//TODO: Funcion para escribir en pantalla de la calculadora
-function numero(number){
-    number.addEventListener('click', () => {
-        valores.textContent += number.textContent;
-    });
-}
-
-//TODO: Cambiar la función para llamar a la operación
-function operacion(a, b){
-    return suma(a, b);
-}
-
-function suma(a, b){
-    return a + b;
-}  
-
-function resta(a, b){
-    return a - b;
-}
-
-function multiplicar(a, b){
-    return a * b;
-}
-
-function dividir(a, b){
-    return a / b;
+let operador = o.textContent;
+  o.addEventListener('click', () => {
+    
+    valores.textContent = "";
+    return operador;
+  })
 }
